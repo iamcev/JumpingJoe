60777d38a1b8fd6ccf024dc1ef934ce583afa40d
Craftingexpert1 <dev.velp@gmail.com>
2018-03-21 17:23:38 -0400

    Yay
    Yay

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Acceleration.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Acceleration.cs
@@ -8,7 +8,8 @@ public class Acceleration : MonoBehaviour {
     public float dc = -0.08f;
     public float v = 0.00f;
     public int t = 0;
-
+	public float q = -6;
+	public GameObject JumpingJoe; 
     public bool Accelerating {
         get
         {
@@ -17,7 +18,8 @@ public class Acceleration : MonoBehaviour {
     }
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	}
 
     // Update is called once per frame
@@ -31,8 +33,8 @@ public class Acceleration : MonoBehaviour {
         {
             t--;
         }
-        var z = gameObject.name.Equals("Main Camera") ? 25f : 0f;
-		var movHoriz = -(Input.GetAxis ("Horizontal"));
-		transform.position = new Vector3 (movHoriz, (v + (a) * t * t) / 10, z);
+		var movHoriz = -(Input.GetAxis("Horizontal"));
+		var z = gameObject.name.Equals ("Main Camera") ? 25f : 0f;
+		transform.position = new Vector3 (movHoriz, (v + (a) * t * t) / 10 - q, z + q);
     }
 }
