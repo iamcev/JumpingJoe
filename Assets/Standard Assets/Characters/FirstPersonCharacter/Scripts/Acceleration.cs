7d161de5c96e83e83b5d9dbe10b5fd5ca0ecf1bc
Craftingexpert1 <dev.velp@gmail.com>
2018-03-22 14:17:20 -0400

    Text
    Text

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Acceleration.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Acceleration.cs
@@ -1,20 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI; 
 public class Acceleration : MonoBehaviour {
-
+	public Text meters; 
     public float a = 0.18f;
     public float dc = -0.08f;
     public float v = 0.00f;
     public int t = 0;
 	public float q = -6;
-	public GameObject JumpingJoe; 
     public bool Accelerating {
         get
         {
             return Input.GetKey("space");
-        }
+		}
     }
 
 	// Use this for initialization
@@ -36,5 +35,10 @@ public class Acceleration : MonoBehaviour {
 		var movHoriz = -(Input.GetAxis("Horizontal"));
 		var z = gameObject.name.Equals ("Main Camera") ? 25f : 0f;
 		transform.position = new Vector3 (movHoriz, (v + (a) * t * t) / 10 - q, z + q);
+		meters.text = t.ToString() + "m";
+		if (t > 1000)
+		{
+			meters.text = (t / 1000).ToString() + "km"; 
+		}
     }
 }
