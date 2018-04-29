863866e17ef1819e5da2092462ba069c3104ce68
Miracle Zhou <msdotnetclr@gmail.com>
2018-04-29 19:59:27 -0400

    add camera follow function

---
+++ Assets/Scenes/jumpinjoe/Scripts/Acceleration.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+public class Acceleration : MonoBehaviour {
+    public Text meters;
+    float delta = 0f;
+    float height = 0f;
+    const float incr = 0.02f;
+    bool Accelerating
+    {
+        get
+        {
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                if (Input.touchCount > 0)
+                {
+                    return true;
+                }
+            }
+            return Input.GetKey("space");
+        }
+    }
+
+    short LeftRightModifier
+    {
+         get
+        {
+            if(Input.GetKey("left"))
+            {
+                return 1;
+            }
+            else if (Input.GetKey("right"))
+            {
+                return -1;
+            }
+            return 0;
+        }
+    }
+
+    public GameObject JumpingJoe;
+
+	// Use this for initialization
+	void Start () 
+	{
+        Restart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.Accelerating)
+        {
+            delta += incr;
+        }
+        else
+        {
+            delta -= incr;
+        }
+        height += delta;
+        if (height <= 0)
+        {
+            height = 0f;
+            delta = 0f;
+        }
+        //Vector3 movHoriz = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var z = gameObject.name.Equals("Main Camera") ? 25f : 0f;
+        var x = transform.position.x + LeftRightModifier * incr * 5;
+        if (x > 17)
+        {
+            x = 17;
+        } else if (x < -17)
+        {
+            x = -17;
+        }
+        transform.position = new Vector3(x, height, transform.position.z);
+		meters.text = height.ToString() + "m";
+        Debug.Log(transform.position);
+		if (height > 1000)
+		{
+			meters.text = (height / 1000).ToString() + "km"; 
+		}
+		
+
+    }	
+
+	void OnCollisionEnter (Collision col) {
+		if (col.gameObject.tag == "Obstacle") {
+			Restart();
+			Debug.Log("flajf");
+		} 
+	}
+
+	void Restart() {
+        delta = 0f;
+        height = 0f;
+        transform.position = new Vector3 (0, 0, 0);
+	} 
+
+}
---
+++ Assets/Scenes/jumpinjoe/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public GameObject Joe;
+    private Vector3 offset;
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - Joe.transform.position;
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = Joe.transform.position + offset;
+    }
+}
