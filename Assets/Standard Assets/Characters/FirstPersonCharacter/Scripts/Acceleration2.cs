271c16ff0da161cd32114aaaf86b423860f97837
Miracle Zhou <msdotnetclr@gmail.com>
2018-04-29 10:22:55 -0400

    made new code for acceleration/deceleration, now accelerates downward, floats in midair for a moment if space is released, TODO: copy scripts together and add back height counter

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Acceleration2.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Acceleration2.cs
@@ -4,12 +4,44 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Acceleration2 : MonoBehaviour {
-
+    float delta = 0f;
+    float height = 0f;
+    const float incr = 0.02f;
+    bool Accelerating
+    {
+        get
+        {
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                if (Input.touchCount > 0)
+                {
+                    return true;
+                }
+            }
+            return Input.GetKey("space");
+        }
+    }
     // Use this for initialization
     void Start () {
+        delta = 0f;
+        height = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if(this.Accelerating)
+        {
+            delta += incr;
+        } else
+        {
+            delta -= incr;
+        }
+        height += delta;
+        if (height <= 0)
+        {
+            height = 0f;
+            delta = 0f;
+        }
+        transform.position = new Vector3(transform.position.x, height, transform.position.z);
     }
 }
